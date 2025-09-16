64aa63ae72399e292214779bf28d8ec1e4128061
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-09-17 00:35:07 +0300

    feat: добавляет пропы item

--- src/shared/components/VacancyCard/VacancyCard.tsx
+++ src/shared/components/VacancyCard/VacancyCard.tsx
@@ -6,13 +6,38 @@ interface VacancyCardProps {
     area: {
       name: string;
     };
+    salary: {
+      from?: number;
+      to?: number;
+      currency?: string;
+    };
+    experience: {
+      name: string;
+    };
+    employer: {
+      name: string;
+    };
+    schedule: {
+      name: string;
+    };
   };
 }
 
 export const VacancyCard = ({ item }: VacancyCardProps) => {
   return (
     <li className={styles["vacancy-card__item"]}>
-      {item.name}: {item.area.name}
+      <p>{item.name}</p>
+      <span>{item.salary && item.salary.from}</span> -{" "}
+      <span>{item.salary && item.salary.to}</span>{" "}
+      {item.salary && item.salary.currency}
+      <p>{item.experience.name}</p>
+      <p>{item.employer.name}</p>
+      <p>{item.schedule.name} - ?</p>
+      <p>{item.area.name}</p>
+      <div>
+        <button>Смотреть вакансию</button>
+        <button>Откликнуться</button>
+      </div>
     </li>
   );
 };
